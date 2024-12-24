25b9c8c031c4e230d42e1f55f880d2dec24c3f94
Ngo Anh Quan <quanngo018@gmail.com>
2024-12-24 11:23:00 +0700

    Add Menu & Merge Add Event, Reminder

--- C++.cpp
+++ C++.cpp
@@ -1,99 +1,116 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <queue>
 #include <cstdio>
-#include <sstream>
+#include <iomanip>
 using namespace std;
 
 class Event {
-    private:
-        string name;
-        int hours;
-        int minutes;
-        int priority;
-        string reminder;
+private:
+    string name;
+    int time;
+    int priority;
+    string reminder;
+    
+    static string formatTime(int time) {
+        int hour = time / 100;
+        int minute = time % 100;
+        return to_string(hour) + " " + (minute < 10 ? "0" : "") + to_string(minute);
+    }
+public:
+    Event(string n, int t, int p, string r) {
+        name = n;
+        time = t;
+        priority = p;
+        reminder = r;
+    }
 
-    public:
-        Event(string n, int h, int m, int p, string r) {
-            name = n;
-            hours = h;
-            minutes = m;
-            priority = p;
-            reminder = r;
+    string getName() const {
+        return name;
+    }
+    int getTime() const {
+        return time;
+    }
+    int getPriority() const {
+        return priority;
+    }
+    string getReminder() const {
+        return reminder;
+    }
+
+    static void display(const queue<Event>& events) {
+        if (events.empty()) {
+            cout << "No events to display." << endl;
+            return;
+        }
+        cout << "+---------------------------------------------------------------------------+" << endl;
+        cout << "|   Name         |       Time      |  Priority  |          Reminder         |" << endl;
+        cout << "+---------------------------------------------------------------------------+" << endl;
+        
+        queue<Event> tempQueue = events;
+        while (!tempQueue.empty()) {
+            Event event = tempQueue.front();
+            tempQueue.pop();
+            printf("| %-14s | %-15s | %-10d | %-25s |\n",
+                   event.getName().c_str(),
+                   formatTime(event.getTime()).c_str(),
+                   event.getPriority(),
+                   event.getReminder().c_str());
         }
 
-        string getName() const {
-            return name;
-        }
-
-        int getHours() const {
-            return hours;
-        }
-
-        int getMinutes() const {
-            return minutes;
-        }
-
-        int getPriority() const {
-            return priority;
-        }
-
-        string getReminder() const {
-            return reminder;
-        }
-
-        void setName(string n) {
-            name = n;
-        }
-
-        void setTime(int h, int m) {
-            hours = h;
-            minutes = m;
-        }
-
-        void setPriority(int p) {
-            priority = p;
-        }
-
-        void setReminder(string r) {
-            reminder = r;
-        }
-
-        static string formatTime(int hours, int minutes) {
-            return to_string(hours) + " " + (minutes < 10 ? "0" + to_string(minutes) : to_string(minutes));
-        }
-
-        static void display(const vector<Event>& events) {
-            if (events.empty()) {
-                cout << "No events to display." << endl;
-                return;
-            }
-            cout << "+---------------------------------------------------------------------------+" << endl;
-            cout << "|      Name      |       Time      |  Priority  |          Reminder         |" << endl;
-            cout << "+---------------------------------------------------------------------------+" << endl;
-            for (const auto& event : events) {
-                printf("| %-14s | %-15s | %-10d | %-25s |\n",
-                       event.getName().c_str(),
-                       formatTime(event.getHours(), event.getMinutes()).c_str(),
-                       event.getPriority(),
-                       event.getReminder().c_str());
-            }
-            cout << "+---------------------------------------------------------------------------+" << endl;
-        }
+        cout << "+---------------------------------------------------------------------------+" << endl;
+    }
 };
 
 int main() {
+    queue<Event> eventQueue;  
+    int choice;
+
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add Event\n";
+        cout << "2. Display Events\n";
+        cout << "3. Handle Conflicts\n";
+        //cout << "4. Add Reminder\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+        
+        if (choice == 1) {
+            string name, reminder;
+            int time, priority;
+            
+            cout << "Enter event name: ";
+            getline(cin, name);
+            cout << "Enter event time: ";
+            cin >> time;
+            cout << "Enter event priority: ";
+            cin >> priority;
+            cin.ignore();  
+            cout << "Enter reminder: ";
+            getline(cin, reminder);
+
+            Event newEvent(name, time, priority, reminder);
+            eventQueue.push(newEvent);  
+            cout << "Event added to queue." << endl;
+        }
+        else if (choice == 2) {
+            Event::display(eventQueue);  
+        }
+        else if (choice == 3) {
+            cout << " " << endl;
+        }
+        else if (choice == 4) {
+            cout << " " << endl;
+        }
+        else if (choice == 5) {
+            cout << "Exiting program." << endl;
+        }
+        else {
+            cout << "Invalid choice, please try again." << endl;
+        }
+    } while (choice != 5); 
     
-    string name, reminder;
-    int hour, minute, priority;
-
-    
-    Event event1("Di choi voi ny", 22, 0, 1, "Ve nha truoc 23 00");
-    Event event2("Hoc XSTK", 20, 0, 2, "Lam BTVN");
-
-    vector<Event> events = {event1, event2};
-
-    Event::display(events);
-
     return 0;
 }
